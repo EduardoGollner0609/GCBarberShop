e6422cbc6678d736c78321688b46ffc763cf6de2
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-21 17:50:14 -0300

    Criando componentes SectionHaircust.

---
+++ src/components/SectionHaircuts/index.tsx
@@ -0,0 +1,10 @@
+import ExampleHaircuts from "../ExampleHaircuts";
+import "./styles.css";
+
+export default function SectionHaircuts() {
+  return (
+    <section className="section-haircuts">
+      <ExampleHaircuts />
+    </section>
+  );
+}
