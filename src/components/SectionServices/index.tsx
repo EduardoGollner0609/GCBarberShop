a4ee230e5d1607e30a6fd5f879eba70095b50e28
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-21 17:49:25 -0300

    Criando componente SectionServices.

---
+++ src/components/SectionServices/index.tsx
@@ -0,0 +1,11 @@
+import ServicesOptions from "../ServicesOptions";
+import "./style.css";
+
+export default function SectionServices() {
+  return (
+    <section id="services-section">
+      <h2>Serviços</h2>
+      <ServicesOptions />
+    </section>
+  );
+}
