0844faaea5b68643f39e27cc44f4a9264a648c11
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-21 17:49:48 -0300

    Criando componente Sectionlocation.

---
+++ src/components/SectionLocation/index.tsx
@@ -0,0 +1,10 @@
+import Location from "../Location";
+import "./styles.css";
+
+export default function SectionLocation() {
+  return (
+    <section id="section-location">
+     < Location />
+    </section>
+  );
+}
