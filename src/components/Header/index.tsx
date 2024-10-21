b5dd7b80b514c409f61bf13a0b01a29eb02722b2
Eduardo Sousa Gollner <dudugollner@gmail.com>
2024-10-21 17:44:20 -0300

    Criando componente Header.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,86 @@
+import "./styles.css";
+import imgLogo from "../../assets/GCBarber-Logo.png";
+import menuItem from "../../assets/menu-items-white.svg";
+import closeMenuItem from "../../assets/close-items-white.svg";
+import { Link } from "react-router-dom";
+import InformationContact from "../InformationContact";
+
+export default function Header() {
+  function menuShow() {
+    const menuMobile = document.querySelector(
+      ".mobile-menu"
+    ) as HTMLImageElement | null;
+    const icon = document.querySelector(".icon") as HTMLImageElement | null;
+
+    if (menuMobile?.classList.contains("open")) {
+      menuMobile.classList.remove("open");
+      icon?.setAttribute("src", menuItem);
+    } else {
+      menuMobile?.classList.add("open");
+      icon?.setAttribute("src", closeMenuItem);
+    }
+  }
+
+  function contactsShow() {
+    const sectionContacts = document.querySelector(
+      ".informations-contact-container"
+    );
+    sectionContacts?.classList.add("active");
+  }
+
+  return (
+    <>
+      <header>
+        <nav className="nav-bar container">
+          <div className="logo">
+            <Link to="/">
+              <img src={imgLogo} alt="Logo" />
+            </Link>
+          </div>
+
+          <div className="list-navegation">
+            <ul>
+              <li className="nav-item">
+                <Link to="/">Inicio</Link>
+              </li>
+              <li className="nav-item">
+                <a href="#services-section">Serviços</a>
+              </li>
+              <li className="nav-item">
+                <a href="#section-location">Endereço</a>
+              </li>
+              <li className="nav-item">
+                <a onClick={contactsShow}>Fale Conosco</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="mobile-menu-icon">
+            <button onClick={menuShow}>
+              <img className="icon" src={menuItem} alt="Abrir Opções" />
+            </button>
+          </div>
+        </nav>
+
+        <div className="mobile-menu">
+          <ul>
+            <li className="nav-item">
+              <Link to="/">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <a href="#services-section">Serviços</a>
+            </li>
+            <li className="nav-item">
+              <a href="#section-location">Endereço</a>
+            </li>
+            <li className="nav-item">
+              <a onClick={contactsShow}>Fale Conosco</a>
+            </li>
+          </ul>
+        </div>
+      </header>
+
+      <InformationContact />
+    </>
+  );
+}
