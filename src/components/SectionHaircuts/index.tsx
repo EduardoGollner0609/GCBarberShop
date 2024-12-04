d49057635b47f25890426f86962b62a0d6bf7228
EduGollner0609 <dudugollner@gmail.com>
2024-12-04 05:28:13 -0300

    Adicionando Swiper ao projeto e efeito do Carrosel com slides.

--- src/components/SectionHaircuts/index.tsx
+++ src/components/SectionHaircuts/index.tsx
@@ -1,10 +1,50 @@
-import ExampleHaircuts from "../ExampleHaircuts";
 import "./styles.css";
+import imgSlide1 from '../../assets/corte-de-cabelo-1.jpg';
+import imgSlide2 from '../../assets/corte-de-cabelo-7.jpg';
+import imgSlide3 from '../../assets/corte-de-cabelo-3.jpg';
+import imgSlide4 from '../../assets/corte-de-cabelo-4.jpg';
+import imgSlide5 from '../../assets/corte-de-cabelo-6.jpg';
+import { SwiperProps, SwiperSlide } from "swiper/react";
+import Slider from "../Slider/slider";
 
 export default function SectionHaircuts() {
+
+  const settings: SwiperProps = {
+    spaceBetween: 50,
+    slidesPerView: 1,
+    navigation: true,
+    pagination: {
+      clickable: true
+    }
+  }
+
   return (
     <section className="section-haircuts">
-      <ExampleHaircuts />
+      <div className="haircuts-examples-slide container">
+        <Slider settings={settings}>
+          <SwiperSlide>
+            <img src={imgSlide1} alt="Corte de Cabelo" />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img src={imgSlide2} alt="Corte de Cabelo" />
+          </SwiperSlide>
+
+
+          <SwiperSlide>
+            <img src={imgSlide3} alt="Corte de Cabelo" />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img src={imgSlide4} alt="Corte de Cabelo" />
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <img src={imgSlide5} alt="Corte de Cabelo" />
+          </SwiperSlide>
+        </Slider>
+      </div>
+
     </section>
   );
 }
